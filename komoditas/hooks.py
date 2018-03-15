2ba25b9f40f382bc744769bb2028927cfcf1c9f4
saint2228 <34730824+saint2228@users.noreply.github.com>
2018-03-15 16:41:00 +0700

    Update hooks.py
    Open link CSS

--- komoditas/hooks.py
+++ komoditas/hooks.py
@@ -15,7 +15,7 @@ app_license = "MIT"
 # ------------------
 
 # include js, css files in header of desk.html
-# app_include_css = "/assets/komoditas/css/komoditas.css"
+app_include_css = "/assets/komoditas/css/komoditas.css"
 # app_include_js = "/assets/komoditas/js/komoditas.js"
 
 # include js, css files in header of web template
